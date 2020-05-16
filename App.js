808335212dbbd538dd978d7610134c35d678cf01
Abdul Badru <abdul.badru@gmail.com>
2020-05-16 15:45:16 +0200

    feat: setup redux store

--- App.js
+++ App.js
@@ -1,36 +1,33 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import * as React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-
-import useCachedResources from './hooks/useCachedResources';
-import BottomTabNavigator from './navigation/BottomTabNavigator';
-import LinkingConfiguration from './navigation/LinkingConfiguration';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import * as React from "react";
+import { Platform, StatusBar, StyleSheet, View } from "react-native";
+import BottomTabNavigator from "./navigation/BottomTabNavigator";
+import LinkingConfiguration from "./navigation/LinkingConfiguration";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers";
 
 const Stack = createStackNavigator();
 
 export default function App(props) {
-  const isLoadingComplete = useCachedResources();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
+  return (
+    <Provider store={createStore(reducer)}>
       <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}
+        {Platform.OS === "ios" && <StatusBar barStyle="dark-content" />}
         <NavigationContainer linking={LinkingConfiguration}>
           <Stack.Navigator>
             <Stack.Screen name="Root" component={BottomTabNavigator} />
           </Stack.Navigator>
         </NavigationContainer>
       </View>
-    );
-  }
+    </Provider>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
 });
---
+++ reducers/index.js
@@ -0,0 +1,37 @@
+import { LOAD_DECKS, ADD_DECK, ADD_CARD } from "../actions";
+
+function decks(state = {}, action) {
+  switch (action.type) {
+    case LOAD_DECKS:
+      return {
+        ...state,
+        ...action.decks,
+      };
+
+    case ADD_DECK:
+      return {
+        ...state,
+        [action.deckTitle]: {
+          title: action.deckTitle,
+          questions: [],
+        },
+      };
+
+    case ADD_CARD: {
+      const out = {
+        ...state,
+      };
+      if (out[action.deckTitle]) {
+        const { question, answer } = action.card;
+        out[action.deckTitle].questions.push({ question, answer });
+      }
+      return out;
+      break;
+    }
+
+    default:
+      return state;
+  }
+}
+
+export default decks;
